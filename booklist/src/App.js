91db68097f002d2aacb2b99739c416d9bc8d4a51
jewells <jewellsjoshi437@gmail.com>
2020-08-18 13:12:45 +0530

    17-Reading-List-Project(2)

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
+import Booklist from './components/BookList';
 function App() {
   return (
     <div className="App">
      <BookContextProvider>
         <Navbar/>
+        <Booklist/>
      </BookContextProvider>
     </div>
   );
