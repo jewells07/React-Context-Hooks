987f3c83c6126b660f9a4caa1ff3d325ecafb3f6
jewells <jewellsjoshi437@gmail.com>
2020-08-18 13:52:17 +0530

    20-Adding-a-Reducer

--- booklist/src/components/BookForm.js
+++ booklist/src/components/BookForm.js
@@ -2,16 +2,16 @@ import React, { useState, useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const NewBookForm = () => {
-  const { addBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
-  const handleSubmit =(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    addBook(title, author);
-    setTitle('')
-    setAuthor('')
-  }
+    dispatch({ type: 'ADD_BOOK', book: { title, author } });
+    setTitle('');
+    setAuthor('');
+  };
 
   return (
     <form onSubmit={handleSubmit}>
